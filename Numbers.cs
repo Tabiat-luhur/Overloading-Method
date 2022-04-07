46fa7f97f2b9b1b0b8e757d2d7681ecbdbdad724
Farhan Purnama Putra <farhanpurnamaputra313@gmail.com>
2022-04-07 21:51:30 +0700

    Tugas Pemrograman Over Loading Pada Method

---
+++ Numbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    
+    public class Numbers
+    {
+        private int _n1, _n2;
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            _n1 = number1 < number3 ? number1 : (int)number3;
+            _n2 = number2 < number3 ? number2 : (int)number3;
+            return number1 < number2 ? _n1 : _n2;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            _n1 = number1 > number3 ? number1 : (int)number3;
+            _n2 = number2 > number3 ? number2 : (int)number3;
+            return number1 > number2 ? _n1 : _n2;
+        }
+    }
+}
